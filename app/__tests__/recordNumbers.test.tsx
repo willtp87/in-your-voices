2c3283ccf9892ee4648696e1cf87f0d54b40b0e8
William Panting <willtp87@hotmail.com>
2024-08-20 16:08:40 -0300

    feat: Added numbers recording screen.

---
+++ app/__tests__/recordNumbers.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react-native";
+import React from "react";
+import { Provider } from "react-redux";
+
+import { store } from "../../store";
+import RecordNumbers from "../recordNumbers";
+
+describe("<RecordNumbers />", () => {
+  it("has expected number of children", () => {
+    const tree: any = render(
+      <Provider store={store}>
+        <RecordNumbers />
+      </Provider>,
+    ).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
+});
--- app/recordNumbers.tsx
+++ app/recordNumbers.tsx
@@ -1,5 +1,5 @@
-import { Text, Button } from "@rneui/themed";
-import { useNavigation, useRouter } from "expo-router";
+import { Text } from "@rneui/themed";
+import { useNavigation } from "expo-router";
 import React, { useEffect } from "react";
 import "../i18n/i18n";
 import { useTranslation } from "react-i18next";
@@ -9,11 +9,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppSelector } from "../store/hooks";
 import { selectManagingVoice } from "../store/voicesSlice";
 
-// Specific voice portal.
+// Screen to record numbers screen.
 export default function Page() {
   const { t } = useTranslation();
   const navigation = useNavigation();
-  const router = useRouter();
   const managingVoice = useAppSelector(selectManagingVoice);
 
   useEffect(() => {
@@ -23,10 +22,6 @@ export default function Page() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>{managingVoice?.desc}</Text>
-      <Button
-        title={t("numbersTitle")}
-        onPress={() => router.push("/recordNumbers")}
-      />
     </SafeAreaView>
   );
 }
@@ -35,6 +30,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
 });
