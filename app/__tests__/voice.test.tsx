ec4d975e5fc405cc315dd5b4c6311445a6d70ff6
William Panting <willtp87@hotmail.com>
2024-08-24 14:16:28 -0300

    test: Eliminating unnecessary testing.

--- app/__tests__/voice.test.tsx
+++ app/__tests__/voice.test.tsx
@@ -1,42 +1,23 @@
-import { render, userEvent } from "@testing-library/react-native";
+import { userEvent } from "@testing-library/react-native";
 import { renderRouter, screen } from "expo-router/testing-library";
 import React from "react";
-import { View } from "react-native";
 import { Provider } from "react-redux";
 
 import { store } from "../../store";
 import Voice from "../voice";
 
 describe("<Voice />", () => {
-  it("has expected number of children", () => {
-    const tree: any = render(
-      <Provider store={store}>
-        <Voice />
-      </Provider>,
-    ).toJSON();
-    expect(tree.children.length).toBe(2);
-  });
   it("can navigate to recordNumbers", async () => {
     const user = userEvent.setup();
 
-    const mockVoice = () => (
-      <Provider store={store}>
-        <Voice />
-      </Provider>
-    );
-    const mockView = () => <View />;
+    renderRouter({
+      index: () => (
+        <Provider store={store}>
+          <Voice />
+        </Provider>
+      ),
+    });
 
-    renderRouter(
-      {
-        voice: mockVoice,
-        recordNumbers: mockView,
-      },
-      {
-        initialUrl: "/voice",
-      },
-    );
-
-    expect(screen).toHavePathname("/voice");
     await user.press(screen.getByTestId("numbers"));
     expect(screen).toHavePathname("/recordNumbers");
   });
