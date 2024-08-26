55701a19edc9e3a4e2d5393d6cf7518defb44bef
William Panting <willtp87@hotmail.com>
2024-08-25 21:15:12 -0300

    test: Some tests don't need stores.

--- components/Circles/index.test.tsx
+++ components/Circles/index.test.tsx
@@ -1,17 +1,11 @@
 import { render } from "@testing-library/react-native";
 import React from "react";
-import { Provider } from "react-redux";
 
 import Circles from ".";
-import { store } from "../../store";
 
 describe("<Circles />", () => {
   it("generates expected number of circles", () => {
-    const tree: any = render(
-      <Provider store={store}>
-        <Circles count={5} />
-      </Provider>,
-    ).toJSON();
+    const tree: any = render(<Circles count={5} />).toJSON();
     expect(tree.children.length).toBe(5);
   });
 });
--- components/Number/index.test.tsx
+++ components/Number/index.test.tsx
@@ -1,16 +1,11 @@
 import { render, screen } from "@testing-library/react-native";
 import React from "react";
-import { Provider } from "react-redux";
 
 import Number from ".";
-import { store } from "../../store";
+
 describe("<Number />", () => {
   it("has expected text", () => {
-    render(
-      <Provider store={store}>
-        <Number num={0} word="Zero" />
-      </Provider>,
-    );
+    render(<Number num={0} word="Zero" />);
     expect(screen.getByText("0"));
     expect(screen.getByText("Zero"));
   });
