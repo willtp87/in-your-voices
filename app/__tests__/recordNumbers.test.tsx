fdb73c1910a9cf6552007057f0c7b05bf4b41869
William Panting <willtp87@hotmail.com>
2024-08-27 16:33:46 -0300

    test: Testing voice page respects state.

--- app/__tests__/recordNumbers.test.tsx
+++ app/__tests__/recordNumbers.test.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import { Provider } from "react-redux";
 
 import { store } from "../../store";
+import { createVoice, updateVoice } from "../../store/voices";
+import { setManagingVoice } from "../../store/voicesSlice";
 import RecordNumbers from "../recordNumbers";
-
 describe("<RecordNumbers />", () => {
   it("has number recorders", () => {
     render(
@@ -14,5 +15,20 @@ describe("<RecordNumbers />", () => {
     );
     screen.getByTestId("mic0");
     screen.getByTestId("mic9");
+  });
+  it("uses current voice", () => {
+    const dispatch = store.dispatch;
+    const voice = { dir: "./voices/0", title: "title", desc: "desc" };
+    dispatch(createVoice());
+    dispatch(setManagingVoice(voice));
+    dispatch(
+      updateVoice({ dir: voice.dir, title: voice.title, desc: voice.desc }),
+    );
+    render(
+      <Provider store={store}>
+        <RecordNumbers />
+      </Provider>,
+    );
+    screen.getByText("desc");
   });
 });
