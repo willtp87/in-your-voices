c35dc312e4ec24c8030a8d1b875bd4e400f7e11a
William Panting <willtp87@hotmail.com>
2024-09-08 15:57:26 -0300

    test: Checking button visibility.

--- app/__tests__/recordNumbers.test.tsx
+++ app/__tests__/recordNumbers.test.tsx
@@ -7,18 +7,22 @@ import { createVoice } from "../../store/voices";
 import { setManagingVoice } from "../../store/voicesSlice";
 import RecordNumbers from "../recordNumbers";
 describe("<RecordNumbers />", () => {
-  it("has number recorders", () => {
-    render(
-      <Provider store={store}>
-        <RecordNumbers />
-      </Provider>,
-    );
-    screen.getByTestId("mic0");
-    screen.getByTestId("mic9");
-  });
-  it("uses current voice", () => {
+  it("uses a voice with no records", async () => {
+    /**
+     * @xxx Running into testing problems with recordings.
+     * Failed to start recording TypeError: Cannot read properties of undefined (reading 'uri')
+     *  at Recording.uri (/home/wpanting/rn-dev/in-your-voices/node_modules/expo-av/src/Audio/Recording.ts:327:9)
+     *  at Generator.next (<anonymous>)
+     *  at asyncGeneratorStep (/home/wpanting/rn-dev/in-your-voices/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:17)
+     *  at _next (/home/wpanting/rn-dev/in-your-voices/node_modules/@babel/runtime/helpers/asyncToGenerator.js:17:9)
+     */
     const dispatch = store.dispatch;
-    const voice = { dir: "./voices/0", title: "title", desc: "desc" };
+    const voice = {
+      dir: "./voices/0",
+      title: "title",
+      desc: "desc",
+      numberRecordings: {},
+    };
     dispatch(createVoice());
     act(() => {
       dispatch(setManagingVoice(voice));
@@ -28,6 +32,10 @@ describe("<RecordNumbers />", () => {
         <RecordNumbers />
       </Provider>,
     );
+    screen.getByTestId("mic0");
+    screen.getByTestId("mic9");
     screen.getByText("desc");
+    expect(await screen.queryByTestId("stop0")).toBeFalsy();
+    expect(await screen.queryByTestId("play0")).toBeFalsy();
   });
 });
