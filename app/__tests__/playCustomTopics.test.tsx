cc192891e2df2fe9fe2c49695952138d359dc0dd
William Panting <willtp87@hotmail.com>
2024-12-07 14:28:33 -0400

    test: Basic render tests for now.

--- app/__tests__/playCustomTopics.test.tsx
+++ app/__tests__/playCustomTopics.test.tsx
@@ -1,12 +1,14 @@
-import { renderRouter } from "expo-router/testing-library";
+import {
+  renderRouter,
+} from "expo-router/testing-library";
 import React from "react";
 import { Provider } from "react-redux";
 
 import { store } from "../../store";
 import { createTopic, getTopics } from "../../store/customTopics";
-import RecordCustomTopics from "../recordCustomTopics";
+import PlayCustomTopics from "../playCustomTopics";
 
-describe("<RecordCustomTopics />", () => {
+describe("<PlayCustomTopics />", () => {
   it("has expected number of children", async () => {
     const dispatch = store.dispatch;
     dispatch(createTopic());
@@ -14,7 +16,7 @@ describe("<RecordCustomTopics />", () => {
     const tree: any = renderRouter({
       index: () => (
         <Provider store={store}>
-          <RecordCustomTopics />
+          <PlayCustomTopics />
         </Provider>
       ),
     }).toJSON();
