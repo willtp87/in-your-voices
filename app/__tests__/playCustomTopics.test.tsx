9f97077788d5bba6832461423cff5774127254ea
William Panting <willtp87@hotmail.com>
2024-12-08 11:15:30 -0400

    test: No need to populate the topics in the test.

--- app/__tests__/playCustomTopics.test.tsx
+++ app/__tests__/playCustomTopics.test.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import { Provider } from "react-redux";
 
 import { store } from "../../store";
-import { createTopic, getTopics } from "../../store/customTopics";
+import { createTopic } from "../../store/customTopics";
 import PlayCustomTopics from "../playCustomTopics";
 
 describe("<PlayCustomTopics />", () => {
   it("has expected number of children", async () => {
     const dispatch = store.dispatch;
     dispatch(createTopic());
-    dispatch(getTopics());
     const tree: any = renderRouter({
       index: () => (
         <Provider store={store}>
